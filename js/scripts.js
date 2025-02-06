e541146fda5fff7cc110c259dd5e11059d08d575
Aszlaczek <awzorek23@wp.pl>
2025-02-06 11:46:01 +0100

    Response for mobile

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,43 @@
 const listOfButtons = document.getElementsByClassName("list-item");
 const contactButton = listOfButtons.item(listOfButtons.length - 1);
+const navbar = document.getElementById("navigation");
+const fullList = document.getElementsByTagName("ul");
+
 const emailPop = document.createElement("a");
 emailPop.innerHTML = "awzorek23@wp.pl";
 emailPop.classList.add("email");
 emailPop.href = "mailto:awzorek23@wp.pl";
 
+const openButton = document.createElement("button");
+openButton.textContent = "Open";
+openButton.classList.add("btn-open");
+
+openButton.addEventListener("click", () => {
+  openButton.classList.toggle("showList");
+  if (openButton.classList.contains("showList")) {
+    openButton.innerText = "Close";
+    fullList[0].classList.add("showMenu");
+    navbar.classList.add("openNav");
+  } else {
+    openButton.innerText = "Open";
+    fullList[0].classList.remove("showMenu");
+    navbar.classList.remove("openNav");
+  }
+});
+
+const showOpen = () => {
+  if (window.innerWidth < 750) {
+    navbar.hasChildNodes(openButton) ? navbar.appendChild(openButton) : "";
+  } else {
+    navbar.hasChildNodes(openButton) ? navbar.removeChild(openButton) : "";
+  }
+};
+window.onload = () => showOpen();
+window.onresize = () => showOpen();
+
 function showEmail() {
-  contactButton.classList.toggle("open");
-  if (contactButton.classList.contains("open")) {
+  contactButton.classList.toggle("show");
+  if (contactButton.classList.contains("show")) {
     emailPop.classList.add("open");
     setTimeout(() => {
       contactButton.appendChild(emailPop);
